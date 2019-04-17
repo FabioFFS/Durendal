f5100a45ffa9beda88c630f6a868c55b1a4b02a7
Fábio Franco da Silva <fabio101949@gmail.com>
2019-04-17 06:52:40 -0300

    1.2

--- Model/DAO/Factory/Connection.php
+++ Model/DAO/Factory/Connection.php
@@ -1,19 +1,37 @@
 <?php
     class Connection{
         public static $instancia;
+        private $user;
+        private $banco;
+        private $senha;
+        private $servidor;
 
         private function __construct(){
-
+            $this->user = "root";
+            $this->banco = "DurendalDB";
+            $this->senha = "";
+            $this->servidor = "localhost";
         }
 
         public static function getInstancia(){
             if(!isset(self::$instancia)){
-                self::$instancia = new PDO('mysql:host=localhost;dbname=DuredalDB','rott','',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+                self::$instancia = new PDO('mysql:host=localhost;dbname=DuredalDB','root','',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
                 
                 self::$instancia->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 self::$instancia->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
             }
             return self::$instancia;
+        }
+        
+        public function conectar(){
+            try{
+                if(is_null(self::$instancia)){
+                    self::$instancia = new PDO('mysql:host='.$this->servidor.';dbname='.$this->banco, $this->user, $this->senha);
+                }
+                return self::$instancia;
+            } catch (PDOException $ex) {
+
+            }
         }
     }
 ?>
---
+++ Model/DAO/Factory/Funcoes.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'Connection.php';
+require 'password.php';
+
+class Funcoes {
+    
+    public function cripto($senha) {
+        return password_hash($senha, PASSWORD_DEFAULT);
+    } 
+}
